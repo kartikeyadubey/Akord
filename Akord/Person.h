8cf115b5b9e2c12ec679a252715b3116ba131fcd
Kartikeya Dubey <kartikeyadubey@gmail.com>
2012-04-29 22:11:36 -0400

    Draw email ids

---
+++ Akord/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Akord
+//
+//  Created by Kartikeya Dubey on 4/29/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Akord/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Akord
+//
+//  Created by Kartikeya Dubey on 4/29/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
